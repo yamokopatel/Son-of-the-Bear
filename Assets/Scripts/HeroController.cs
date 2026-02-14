4a951890aefafca4e20f7327eb2548b2cfe1c5cc
Denis <cvetkovsdeniss4@gmail.com>
2026-02-14 12:49:21 +0200

    Started hero controller

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -2,15 +2,74 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour
 {
+    public float speed;
+    private float a;
+    public float aStep;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        a = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Update is called every 0.02 s
+    void FixedUpdate()
+    {
+        ChangeA();
+    }
+
+    // -- Behaviour functions -- //
+    private void ChangeA()
+    {
+        float rotDirection = OppositeKeys(KeyCode.Q, KeyCode.E);
+        if (rotDirection != 0)
+        {
+            if (a >= -180 && a <= 180)
+            {
+                a += aStep * rotDirection;
+            }
+            else
+            {
+                if (a < 0)
+                {
+                    a = 180 - aStep;
+                }
+                else if (a > 0)
+                {
+                    a = -180 + aStep;
+                }
+            }
+        }
+    }
+
+    // -- Get functions -- //
+    public float GetA()
+    {
+        return a;
+    }
+
+    // -- DRY functions -- //
+    private float OppositeKeys(KeyCode negative,KeyCode positive)
+    {
+        bool nPress = Input.GetKeyDown(negative);
+        bool pPress = Input.GetKeyDown(positive);
+        if(nPress && !pPress)
+        {
+            return -1f;
+        }
+        else if(nPress && pPress)
+        {
+            return 0f;
+        }
+        else if(!nPress && pPress)
+        {
+            return 1f;
+        }
+        return 0f;
     }
 }
